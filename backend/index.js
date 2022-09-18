b52b76b797be7817180a61bb60896a1586c28063
reesehaywood <reese.haywood@gmail.com>
2022-09-18 00:22:17 -0400

    added functionality

--- backend/index.js
+++ backend/index.js
@@ -1,56 +1,128 @@
 const express = require('express');
 const session = require('express-session');
+const fileStore = require('session-file-store')(session);
+
 const port = process.env.PORT || 3000;
 const env = require('dotenv').config();
 const path = require('path');
 
+const {createClient} = require('@supabase/supabase-js')
+
+const supabaseUrl = process.env.SUPABASEURL;
+const supabaseKey = process.env.SUPABASEKEY;
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+let teamString='';
 const DBPATH = process.env.DBPATH;
 const SESSIONSECRET = process.env.SESSIONSECRET;
-var ses;
+
+
+
+leaderList = [
+    {
+        teamName: "Reese",
+        score: 100,
+        asOfDate: Date.now(),
+    },
+    {
+        teamName: "Stephanie",
+        score: 999,
+        asOfDate: Date.now(),
+    }
+]
 
 const app = express()
+var fileStoreOptions = {path: path.join(__dirname,'../.data')};
 
+app.set('trust proxy',1);
 app.use(express.static('frontend'));
 app.use(express.json());
 app.use(
     session({
         secret: SESSIONSECRET,
         resave: false,
-        saveUninitialized: true,
+        saveUninitialized: false,
+
+        store: new fileStore(fileStoreOptions),
         cookie: {
-            path: "/",
-            secure: true,
-            maxAge: 30 * 24 * 60 * 60 * 1000 // e.g. 1 year
+            httpOnly: true,
+            secure: false,
+            maxAge: 30 * 24 * 60 * 60,
         },
     })
 );
 
 app.get('/', (req, res) => {
-    ses = req.session;
-    console.log(ses);
-    if (ses.userid) {
-        res.sendFile(__dirname + '/frontend/index.html');
-    }else{
-        res.json({userid: 'unknown'});
+    console.log('This is running');
+    //var ses = req.session;
+    //console.log(ses);
+    //console.log(req.sessionID)
+    //if (ses.userid) {
+    if(req.session.teamNameJson===undefined){
+        res.sendFile(path.join(__dirname,'../frontend/first.html'));
+    } else {
+        //if already on a team send team name
+        console.log('teamNameJson')
+        console.log(req.session.teamNameJson);
+        console.log('before teamName')
+        teamString = req.session.teamNameJson.teamName;
+        res.redirect('/hasteam?teamName='+teamString)
+        //res.sendFile(path.join(__dirname,'../frontend/second.html'));
     }
+    //}else{
+    //    res.json({userid: 'unknown'});
+    //}
+    //if (req.session.views) {
+    //    req.session.views++;
+    //    res.setHeader('Content-Type', 'text/html');
+    //    res.write('<p>views: ' + req.session.views + '</p>');
+    //    res.end();
+    //  } else {
+    //    req.session.views = 1;
+    //    res.end('Welcome to the file session demo. Refresh page!');
+    //  }
+})
+app.get('/hasteam',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../frontend/first.html'));
+})
+
+app.get('/newTeam',(req,res)=>{
+    req.session.destroy();
+    res.redirect('/');
 })
 
 app.post('/teamName',(req,res)=>{
     console.log(req.body);
     if(req.body.teamName){
-        ses = req.session;
-        ses.teamName = req.body.teamName;
-        console.log(ses);
-        const fileName = path.join(__dirname, '../frontend/index.html');
-        console.log(fileName);
+        req.session.teamName = req.body.teamName;
+        console.log(req.session.teamName);
+        console.log("session id");
+        console.log(req.sessionID)
         const teamNameJson = {
-            teamName: ses.teamName,
+            teamName: req.session.teamName,
         }
+        req.session.teamNameJson = teamNameJson;
         res.json(teamNameJson);
     }
+    //add the session to supabase?
 
+})
+
+app.post('/leaders',async (req,res)=>{
+    
+    const { data: leaderList1, error } = await supabase.from('leaderboard')
+    .select('teamName, score, asOfDate').order('score',{ascending: false})
+    .limit(10);
+
+    console.log(leaderList1);
+    console.log(req.body);
+    res.json(leaderList1);
 })
 
 app.listen(port, () => {
     console.log('Listening on port ' + port);
 })
+
+function getSessionFromSupabase(sessionID){
+
+}
--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,23 @@
 console.log('running client script');
 
+const windowLoad = window.addEventListener('load',event => {
+    console.log('page is fully loaded');
+    const queryString = window.location.search;
+    console.log(queryString);
+    const urlParams = new URLSearchParams(queryString);
+    console.log(urlParams)
+    if(urlParams.has('teamName')){
+        var teamName = urlParams.get('teamName')
+        console.log('teamName from urlparam')
+        console.log(teamName)
+        let teamNameIn = {teamName: teamName};
+        console.log('finish urlparam');
+        showPage(teamNameIn);
+    }
+  }
+  )
 
+let teamNameSave ={teamName: ''};
 const rpsButton = document.getElementById('rpsButton');
 const rpsLink = document.getElementById('rpsLink');
 const tttButton = document.getElementById('tttButton');
@@ -13,13 +30,14 @@ const scissors = document.getElementById('scissors');
 const yourChoiceP = document.getElementById("yourChoiceP");
 const rpsCPU = document.getElementById("rpsCPU");
 const form = document.querySelector('form');
+const leaderList = document.getElementById('leaderList');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const formData = new FormData(form);
-    const teamName = formData.get('teamName');
+    let teamNameText = formData.get('teamNameInput');
     const teamNameJson = {
-        teamName: teamName,
+        teamName: teamNameText,
     }
     console.log(teamNameJson);
     //send data to server
@@ -30,23 +48,87 @@ form.addEventListener('submit', (event) => {
 
     }
     fetch('/teamName', options).then(response => response.json())
-    .then(teaName => {
-        console.log(teamName);
-
-        //now hide the form and unhide the rest?
-        if(teamName){
-            showHideScreen(true,'navBar');
-            showHideScreen(true,'leaderScreen')
-            showHideScreen(true, 'gameButtons');
-            showHideScreen(false, 'teamForm');
-            showHideScreen(true,'teamNameHeader');
-            const teamNameHeader = document.getElementById('teamNameHeader');
-            teamNameHeader.textContent = "Welcome "+teamName+". Play to win!";
-        }
+    .then(teamName2 => {
+        console.log('in 2nd then in fetch')
+        console.log(teamName2);
+        teamNameSave.teamName = teamName2.teamName
+        showPage(teamNameSave)
+        
     })
 
 })
 
+function showPage(teamNameIn){
+
+    // const queryString = window.location.search;
+    // console.log(queryString);
+    // const urlParams = new URLSearchParams(queryString);
+    // console.log(urlParams)
+
+    // if(urlParams.has('teamName')){
+    //     var teamName = urlParams.get('teamName')
+    //     console.log('teamName from urlparam')
+    //     console.log(teamName)
+    //     teamNameIn = {teamName: teamName};
+    //     console.log('finish urlparam')
+    // }
+    //now hide the form and unhide the rest?
+    if(teamNameIn){
+        console.log('logging teamName')
+        console.log(teamNameIn);
+        console.log('finished loggin teamName')
+        getLeaderList()
+        showHideScreen(true,'navBar');
+        showHideScreen(true,'leaderScreen')
+        showHideScreen(true, 'gameButtons');
+        showHideScreen(false, 'teamForm');
+        showHideScreen(true,'teamNameHeader');
+        const teamNameHeader = document.getElementById('teamNameHeader');
+        teamNameHeader.textContent = "Welcome to team "+teamNameIn.teamName+". Play to win!";
+        var diffTeam = document.createElement('a');
+        diffTeam.setAttribute('href','/newTeam');
+        diffTeam.textContent = '(Join a different team!)'
+        teamNameHeader.appendChild(diffTeam)
+    }
+}
+
+function getLeaderList(){
+    //send data to server
+    const options = {
+        method: 'POST',
+        headers: { "content-type": "application/json" },
+    //    body: JSON.stringify(teamNameJson)
+    }
+    fetch('/leaders',options).then(res=>res.json())
+    .then(leaders => {
+        console.log(leaders);
+        leaderList.innerHTML="";
+        leaders.sort((a,b)=> b.score - a.score);
+        console.log(leaders);
+        leaders.forEach(element => {
+            var li = document.createElement('li');
+            li.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-start");
+            var div = document.createElement('div');
+            div.classList.add("ms-2", "me-auto");
+            var div2 = document.createElement('div');
+            div2.classList.add("fw-bold");
+            div2.textContent = element.teamName;
+            div.appendChild(div2);
+            var p = document.createElement('p')
+            p.textContent=Date(element.asOfDate);
+            div.appendChild(p);
+            var span = document.createElement('span');
+            span.classList.add("badge","bg-primary","rounded-pill")
+            span.textContent = element.score;
+            
+            li.appendChild(div);
+            li.appendChild(span);
+            leaderList.appendChild(li);
+        });
+    }
+
+    )
+}
 
 rpsButton.addEventListener('click', rpsClickEvent);
 rpsLink.addEventListener('click', rpsClickEvent);
